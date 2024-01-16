9ecf0864c59842ab3e4d71fca2f248633cb57327
Krasto <alexbuffa22+coder@gmail.com>
2024-01-16 17:07:48 +0000

    minmax: prettified

--- ci_quixo/minmax.py
+++ ci_quixo/minmax.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Literal, Union
 import numpy as np
 from collections import defaultdict
 import random
+import time
 
 if TYPE_CHECKING:
     from custom_game import CompleteMove
@@ -19,6 +20,22 @@ class MinMaxPlayer(Player):
         htable: bool = True,
 
     ) -> None:
+        """Minimax Player
+
+        Args:
+            max_depth (int, optional): Tree depth. Defaults to 2.
+            alpha_beta (bool, optional): Whether to use the Alpha-Beta pruining. Defaults to True.
+            pruning (Literal['0', '1', '2', '3'] , optional): Pruning level. Defaults to 1.
+                This pruning level determines the amount of pre-filtering done to the MinMax tree (i.e. how many children a node has)
+                0: Consider only valid moves
+                1: Consider only valid moves that land on distinct boards (purge moves that would land on a board that is already covered by another move)
+                2: Consider only valid moves that land on distinct *canonical* boards (purge moves that would land on the same equivalence class of already covered boards)
+                3: Same as 2, plus we filter the boards that we have already covered on a lower depth (where the lowest is the root)
+                    This is done because it is possible, with a sufficiently high `max_depth`, to loop into an already covered board, 
+                    and if I have encountered it at a lower depth, it means that that evaluation has more information than I can ever hope to achieve, meaning it's useless 
+                    to expand this subtree
+            htable (bool, optional): Whether to use an hash-table to save and use already evaluated states. Defaults to True.
+        """
         super().__init__()
         
         self.max_depth = 2 if max_depth is None else max_depth
@@ -31,21 +48,27 @@ class MinMaxPlayer(Player):
             str, dict[tuple[Literal["l", "h"], int], float]
         ] = defaultdict(lambda: defaultdict(float))
 
-        self.stats = defaultdict(int)
+        self._stats = defaultdict(int)
 
     def make_move(self, game: Game) -> "CompleteMove":
+        start = time.time()
         cg = CustomGame.from_game(game)
         best_move = self._minmax(cg)
         if best_move is None or not cg.is_valid(best_move):
-            self.stats["EVAL-invalidmove"] += 1
+            self._stats["EVAL-invalidmove"] += 1
             best_move = random.choice(cg.valid_moves())
+        else:
+            self._stats['evals'] += 1
+            self._stats['evals-ms'] += time.time() - start
+            
+        
         return best_move
 
     def search_in_htable(
         self, game: "CustomGame", curr_depth: int, curr_side: Literal["l", "h"]
     ) -> Union[float, None]:
         if not self.use_htable or str(game) not in self.htable:
-            self.stats["HTABLE-MISS"] += 1
+            self._stats["HTABLE-MISS"] += 1
             return None
 
         visited = self.htable[str(game)]
@@ -73,11 +96,11 @@ class MinMaxPlayer(Player):
         dv = sms_dv if sms_dv[0] < cnt_dv[0] else cnt_dv
 
         if dv[0] <= self.max_depth:
-            self.stats["HTABLE-HIT"] += 1
-            self.stats[f"HTABLE-HIT-{dv[0]}/{curr_depth}"] += 1
+            self._stats["HTABLE-HIT"] += 1
+            self._stats[f"HTABLE-HIT-{dv[0]}/{curr_depth}"] += 1
             return dv[1]
         
-        self.stats["HTABLE-MISS"] += 1
+        self._stats["HTABLE-MISS"] += 1
         return None
 
     def put_in_htable(
@@ -95,13 +118,13 @@ class MinMaxPlayer(Player):
         whoami = game.get_current_player()
 
         def moves_getter(game: "CustomGame", depth: int) -> list[tuple["CompleteMove", "CustomGame"]]:
-            self.stats["MOVES-THEORETICAL"] += 44  # length of POSSIBLE_MOVES
+            self._stats["MOVES-THEORETICAL"] += 44  # length of POSSIBLE_MOVES
             if self.pruning_level == 0:
-                moves = POSSIBLE_MOVES
+                moves = game.valid_moves(None, False, False)
             elif self.pruning_level == 1:
                 # filter the moves that land on a board already covered
-                moves = game.valid_moves(None, True, True)
-            else: # both 3 and 4
+                moves = game.valid_moves(None, True, False)
+            else: # both 2 and 3
                 # filter the moves that land on a board already covered (using symmetries)
                 moves = game.valid_moves(None, True, True)
             games = [game.simulate_move(move) for move in moves]
@@ -112,9 +135,9 @@ class MinMaxPlayer(Player):
                 pre = len(move_n_games)
                 move_n_games = [it for it in move_n_games if str(it[1]) not in already_visited]
                 post = len(move_n_games)
-                self.stats["PRUNING4-DIFF"] += pre-post
+                self._stats["PRUNING3-DIFF"] += pre-post # this is always 0.. Is depth 2 enough for a loop? Maybe 3 is
             
-            self.stats["MOVES-ACTUAL"] += len(move_n_games)
+            self._stats["MOVES-ACTUAL"] += len(move_n_games)
             return move_n_games
             
         def min_side(
@@ -128,7 +151,7 @@ class MinMaxPlayer(Player):
 
             winner = game.check_winner()
             if (self.max_depth is not None and depth >= self.max_depth) or winner != -1:
-                score = -1 * game.score # We want the score as if I'm the other player (thus, *-1)
+                score = -1 * game.score # We want the score as if I'm the other player (thus *-1)
                 self.put_in_htable(game, depth, "l", score)
                 return score
 
@@ -175,7 +198,7 @@ class MinMaxPlayer(Player):
         alpha, beta = -np.inf, np.inf
 
         if str(game) in self.history:
-            self.stats["cache-hit"] += 1
+            self._stats["cache-hit"] += 1
             return self.history[str(game)]
 
         # print(f"MAX {whoami=} minning={game.next_move_for}")
@@ -185,14 +208,37 @@ class MinMaxPlayer(Player):
             if min_score > alpha:
                 alpha = min_score
                 best_move = move
-        self.stats["EVALS"] += 1
+        self._stats["EVALS"] += 1
         self.history[str(game)] = best_move
         self.put_in_htable(game, 0, "h", alpha)
         return best_move
 
 
-if __name__ == "__main__":
-    from helper import evaluate, Human
+    @property 
+    def _avg_time(self):
+        if self._stats['evals'] == 0:
+            return 0
+        return self._stats['eval-ms'] / self._stats['evals']
+    
 
-    m = MinMaxPlayer(2, pruning=3, htable=False)
-    evaluate(m, None, 10, True)
+    @property
+    def stats(self) -> dict[str, str]:
+        am, thm = self._stats["MOVES-ACTUAL"], self._stats["MOVES-THEORETICAL"]
+        pp = {
+            "Average time per move": f"{self._avg_time:.2f}s",  
+            f"Pruning lvl. {self.pruning_level} effectiveness": f"{(1-(am/thm)):.2%}",
+            "Total Moves performed": self._stats["evals"]
+        }
+        if self._stats["EVAL-invalidmove"] != 0:
+            pp['Invalid Moves performed'] = self._stats["EVAL-invalidmove"]
+        if self.use_htable:
+            hitratio = self._stats["HTABLE-HIT"] / self._stats['HTABLE-MISS'] + self._stats['HTABLE-HIT']
+            pp["HashTable HitRatio"] = f"{hitratio:.3%}"
+        return pp
+if __name__ == "__main__":
+    from helper import evaluate
+    from pprint import pprint
+
+    mf = MinMaxPlayer(2, pruning=0, htable=False)
+    evaluate(mf, None, 50, True)
+    pprint(mf.stats, sort_dicts=False)
